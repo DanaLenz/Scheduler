dd5ac7a88a6ab5c11a04b2f48138215f6f865082
Dana Lenz <dana.lenz@rwth-aachen.de>
2021-03-24 12:17:35 +0100

    setup wxWidgets

---
+++ UI/taskWindow.cpp
@@ -0,0 +1,5 @@
+//
+// Created by dana on 23.03.21.
+//
+
+#include "taskWindow.h"
---
+++ UI/taskWindow.h
@@ -0,0 +1,18 @@
+//
+// Created by dana on 23.03.21.
+//
+
+#ifndef SCHEDULER_TASKWINDOW_H
+#define SCHEDULER_TASKWINDOW_H
+
+#include <wx/wxprec.h>
+#include <wx/wx.h>
+
+class taskWindow : public wxFrame {
+
+
+
+};
+
+
+#endif //SCHEDULER_TASKWINDOW_H
